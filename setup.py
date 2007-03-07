90e668df464e0ba281baa05023bc378a4f1b4f42
ralmeida <ralmeida@2038079e-dd2a-0410-b554-e13d1092c3cc>
2007-03-07 02:14:11 +0000

    - Fixed URL in Cheeseshop. - Fixed description. - Added ``dap.plugins.netcdf`` dependency. - Updated version to 0.1.1.

--- setup.py
+++ setup.py
@@ -12,20 +12,20 @@ Topic :: Scientific/Engineering
 Topic :: Software Development :: Libraries :: Python Modules
 """
 
-version = '0.1'
+version = '0.1.1'
 
 setup(name = 'topex',
       version = version,
       description = "A very simple library to interpret and load TOPEX/JASON altimetry data",
       long_description = """\
-A very simple library to read the NetCDF altimetry data of TOPEX and JASON-1. The objective here is automatize the procedure to make that dataset ready to use. This include read how many NetCDF files as required, sub-sample by time/period desired and create an accumulated intuitively dictionary of the pertinent data.
+A very simple library to read the NetCDF altimetry data of TOPEX and JASON-1. The objective here is automate the procedure of making the datasets ready to use. This include reading how all the required NetCDF files, sub-sampling by desired time/period and creating an accumulated intuitive dictionary of the pertinent data.
 
-It's not implemented yet but the most interesting feature would be the class TOPEX, which would made very simple and intuitive load, sub-sample and deal with those dataset""",
+Although not yet implemented the most interesting feature would be a "TOPEX" class, which would make very simple and intuitive to load, sub-sample and deal with these datasets.""",
       classifiers=filter(None, classifiers.split("\n")),
       keywords='altimetry, TOPEX, JASON-1, oceanography, Sea Surface Height',
       author = 'Guilherme Castelao',
       author_email = 'guilherme@castelao.net',
-      url = 'http://cheeseshop.python.org/pypi/TOPEX',
+      url = 'http://cheeseshop.python.org/pypi/topex',
       download_url = 'http://cheeseshop.python.org/packages/source/t/topex/topex-0.1.tar.gz',
       license = 'MIT',
       platforms = ['any'],
@@ -33,6 +33,7 @@ It's not implemented yet but the most interesting feature would be the class TOP
       zip_safe=True,
       install_requires=[
           # -*- Extra requirements: -*-
+          'dap.plugins.netcdf',
       ],
       entry_points="""
       # -*- Entry points: -*-
